0c0af364ad75d94ddaf49cfac5e194cac1a1c19a
aweso <aweso@DESKTOP-AFEMMQ7.lan>
2021-04-27 13:29:11 -0700

    commit test

--- FruityNinja/src/Frame.java
+++ FruityNinja/src/Frame.java
@@ -24,7 +24,7 @@ public class Frame extends JPanel implements ActionListener, MouseListener {
 	
 	//loading the images
 	}
-	
+	//hi
 	public static void main(String[] arg) {
 		Frame f = new Frame();
 		
